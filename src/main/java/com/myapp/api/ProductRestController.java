cbee4deeebb43eb0521600fd08ca572fcdf34042
nellyiriberri <nellyiriberri@gmail.com>
2020-06-17 14:29:15 +0200

    Implement some get, post, put and patch methods

--- src/main/java/com/myapp/api/ProductRestController.java
+++ src/main/java/com/myapp/api/ProductRestController.java
@@ -1,21 +1,55 @@
 package com.myapp.api;
 
+import com.myapp.dao.ProductRepository;
 import com.myapp.model.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/products")
 public class ProductRestController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @GetMapping("")
     public List<Product> getAll(){
-        List<Product> myList = Arrays.asList(new Product("Kinder maxi"));
-        System.out.println(myList.get(0).toString());
-        return myList;
+        return productRepository.findAll();
+    }
+
+    @PostMapping("/product")
+    public Product add(@RequestBody Product product){
+        return productRepository.save(product);
+    }
+
+    @PutMapping("/product/{id}")
+    public void edit(@RequestBody Product product, @PathVariable int id) throws Exception{
+        Product p = productRepository.getOne((long) id);
+        if(p==null){
+            throw new Exception("Product not found");
+        }else{
+            p.setId(id);
+            productRepository.save(p);
+        }
+    }
+
+    @PatchMapping("/{id}")
+    public Product partialEdit(@RequestBody Map<String, Object> fields, @PathVariable int id){
+        Product p = productRepository.findById((long) id).get();
+
+        fields.forEach((key, value) -> {
+            Field field = ReflectionUtils.findField(Product.class, key);
+            ReflectionUtils.makeAccessible(field);
+            ReflectionUtils.setField(field, p, value);
+        });
+
+        productRepository.save(p);
+        return p;
     }
 }
--- src/main/java/com/myapp/model/Product.java
+++ src/main/java/com/myapp/model/Product.java
@@ -1,18 +1,21 @@
 package com.myapp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-public class Product {
+public class Product implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
+
+    public Product(){
+
+    }
 
     public Product(String name) {
         this.name = name;
